fc11010ec51eec01becb0f5414afbe77d2197f52
gongzix <149368983+gongzix@users.noreply.github.com>
2024-10-20 22:33:39 +0800

    update

---
+++ src/engine.py
@@ -0,0 +1,121 @@
+import math
+import sys
+from typing import Iterable, Optional
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from timm.data import Mixup
+from timm.utils import ModelEma
+import utils
+import numpy as np
+
+
+
+def contrastive_loss(logits: torch.Tensor) -> torch.Tensor:
+    
+    return nn.functional.cross_entropy(logits, torch.arange(len(logits), device=logits.device))
+
+def clip_loss(similarity: torch.Tensor) -> torch.Tensor:
+    caption_loss = contrastive_loss(similarity)
+    #image_loss = contrastive_loss(similarity.t())
+    #return (caption_loss + image_loss)/2
+    return caption_loss
+
+def ContrastiveLoss(fmri,image):
+
+    logit_scale=torch.tensor([8]).to(fmri.device)
+
+    fmri = fmri / fmri.norm(p=2, dim=-1, keepdim=True)
+    image = image / image.norm(p=2, dim=-1, keepdim=True)
+
+    logit_scale = logit_scale.exp()
+    logits_per_fmri = torch.matmul(fmri, image.t()) * logit_scale
+    #logits_per_fmri = torch.matmul(fmri, image.t())
+    logits_per_image = logits_per_fmri.t()
+    
+    return clip_loss(logits_per_fmri)
+
+
+
+def train_one_epoch(model: torch.nn.Module, 
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
+                    model_ema: Optional[ModelEma] = None, mixup_fn: Optional[Mixup] = None,
+                    set_training_mode=True):
+
+    model.train(set_training_mode)
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 10
+
+    for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
+
+        
+        samples = samples.to(device, non_blocking=True)     
+        targets = targets.to(device, non_blocking=True)
+        outputs = model(samples)
+        loss=ContrastiveLoss(targets,outputs)
+        loss_value = loss.item()
+        
+        if not math.isfinite(loss_value):
+            optimizer.zero_grad()
+            continue
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+        
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        
+    
+        torch.cuda.synchronize(device)
+        if model_ema is not None:
+            model_ema.update(model)
+
+        
+
+        metric_logger.update(loss=loss_value)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+@torch.no_grad()
+def evaluate(data_loader, model, device):
+
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    # switch to evaluation mode
+    model.eval()
+    avg_top1 = 0
+    avg_loss = 0
+    with torch.no_grad():
+        for test_input, target in metric_logger.log_every(data_loader, 10, header):
+            test_input = test_input.to(device, non_blocking=True)
+            target = target.to(device, non_blocking=True)
+
+            # compute output
+            #with torch.cuda.amp.autocast():
+            output = model(test_input)
+            
+            
+            top1=0
+            output = output.flatten(1)
+            output = output / output.norm(p=2, dim=-1, keepdim=True)
+            target = target / target.norm(p=2, dim=-1, keepdim=True)
+
+            
+            similarity = (100.0 * output @ target.T).softmax(dim=-1)
+            #similarity = (100.0 * target @output .T).softmax(dim=-1)
+            
+            for i in range(output.shape[0]):
+                _, indices = similarity[i].topk(1)
+                if indices == i:
+                    top1+=1
+            avg_top1 += top1
+
+    return top1/3
--- src/train_litemind.py
+++ src/train_litemind.py
@@ -10,8 +10,6 @@ import random
 
 from pathlib import Path
 
-from timm.data import Mixup
-from timm.models import create_model
 from timm.scheduler import create_scheduler
 from timm.optim import create_optimizer
 from timm.utils import NativeScaler, ModelEma
@@ -19,12 +17,8 @@ from functools import partial
 import torch.nn as nn
 
 from engine import train_one_epoch, evaluate
-from samplers import RASampler
 import utils
-from gfnet1d import GFNet1dPyramid
-
-import warnings
-warnings.filterwarnings("ignore", message="Argument interpolation should be")
+from dft_backbone import DFTBackbone
 
 class fMRIimageDataset(torch.utils.data.Dataset):
     def __init__(self, x, y):
@@ -45,32 +39,19 @@ def get_args_parser():
     parser.add_argument('--epochs', default=300, type=int)
 
     # Model parameters
-    parser.add_argument('--arch', default='deit_small', type=str,
-                        help='Name of model to train')
-    parser.add_argument('--trial_size', default=24980, type=int, help='fMRI single trial input size')
-
-    parser.add_argument('--drop', type=float, default=0.0, metavar='PCT',
+    parser.add_argument('--drop_rate', type=float, default=0.1, metavar='PCT',
                         help='Dropout rate (default: 0.)')
-    parser.add_argument('--drop-path', type=float, default=0.1, metavar='PCT',
+    parser.add_argument('--drop_path_rate', type=float, default=0.15, metavar='PCT',
                         help='Drop path rate (default: 0.1)')
-
-    parser.add_argument('--model-ema', action='store_true')
-    parser.set_defaults(model_ema=False)
-    parser.add_argument('--model-ema-decay', type=float, default=0.99996, help='')
-    parser.add_argument('--model-ema-force-cpu', action='store_true', default=False, help='')
 
     # Optimizer parameters
     parser.add_argument('--opt', default='adamw', type=str, metavar='OPTIMIZER',
                         help='Optimizer (default: "adamw"')
-    parser.add_argument('--opt-eps', default=1e-8, type=float, metavar='EPSILON',
-                        help='Optimizer Epsilon (default: 1e-8)')
-    parser.add_argument('--opt-betas', default=None, type=float, nargs='+', metavar='BETA',
-                        help='Optimizer Betas (default: None, use opt default)')
-    parser.add_argument('--clip-grad', type=float, default=1, metavar='NORM',
+    parser.add_argument('--clip_grad', type=float, default=1, metavar='NORM',
                         help='Clip gradient norm (default: None, no clipping)')
     parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                         help='SGD momentum (default: 0.9)')
-    parser.add_argument('--weight-decay', type=float, default=0.05,
+    parser.add_argument('--weight-decay', type=float, default=0.1,
                         help='weight decay (default: 0.05)')
     # Learning rate schedule parameters
     parser.add_argument('--sched', default='cosine', type=str, metavar='SCHEDULER',
@@ -98,74 +79,20 @@ def get_args_parser():
                         help='patience epochs for Plateau LR scheduler (default: 10')
     parser.add_argument('--decay-rate', '--dr', type=float, default=0.1, metavar='RATE',
                         help='LR decay rate (default: 0.1)')
+    
+    parser.add_argument('--model-ema', action='store_true')
+    parser.set_defaults(model_ema=False)
+    parser.add_argument('--model-ema-decay', type=float, default=0.99996, help='')
+    parser.add_argument('--model-ema-force-cpu', action='store_true', default=False, help='')
 
-    # Augmentation parameters
-    parser.add_argument('--color-jitter', type=float, default=0.4, metavar='PCT',
-                        help='Color jitter factor (default: 0.4)')
-    parser.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
-                        help='Use AutoAugment policy. "v0" or "original". " + \
-                             "(default: rand-m9-mstd0.5-inc1)'),
-    parser.add_argument('--smoothing', type=float, default=0.1, help='Label smoothing (default: 0.1)')
-    parser.add_argument('--train-interpolation', type=str, default='bicubic',
-                        help='Training interpolation (random, bilinear, bicubic default: "bicubic")')
 
-    parser.add_argument('--repeated-aug', action='store_true')
-    parser.set_defaults(repeated_aug=False)
-
-    # * Random Erase params
-    parser.add_argument('--reprob', type=float, default=0, metavar='PCT',
-                        help='Random erase prob (default: 0.25)')
-    parser.add_argument('--remode', type=str, default='pixel',
-                        help='Random erase mode (default: "pixel")')
-    parser.add_argument('--recount', type=int, default=1,
-                        help='Random erase count (default: 1)')
-    parser.add_argument('--resplit', action='store_true', default=False,
-                        help='Do not random erase first (clean) augmentation split')
-
-    # * Mixup params
-    parser.add_argument('--mixup', type=float, default=0.8,
-                        help='mixup alpha, mixup enabled if > 0. (default: 0.8)')
-    parser.add_argument('--cutmix', type=float, default=1.0,
-                        help='cutmix alpha, cutmix enabled if > 0. (default: 1.0)')
-    parser.add_argument('--cutmix-minmax', type=float, nargs='+', default=None,
-                        help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
-    parser.add_argument('--mixup-prob', type=float, default=1.0,
-                        help='Probability of performing mixup or cutmix when either/both is enabled')
-    parser.add_argument('--mixup-switch-prob', type=float, default=0.5,
-                        help='Probability of switching to cutmix when both mixup and cutmix enabled')
-    parser.add_argument('--mixup-mode', type=str, default='batch',
-                        help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
-
-    # Distillation parameters
-    parser.add_argument('--teacher-model', default='regnety_160', type=str, metavar='MODEL',
-                        help='Name of teacher model to train (default: "regnety_160"')
-    parser.add_argument('--teacher-path', type=str, default='')
-    parser.add_argument('--distillation-type', default='none', choices=['none', 'soft', 'hard'], type=str, help="")
-    parser.add_argument('--distillation-alpha', default=0.5, type=float, help="")
-    parser.add_argument('--distillation-tau', default=1.0, type=float, help="")
-
-    # * Finetuning params
-    parser.add_argument('--finetune', default='', help='finetune from checkpoint')
-
-    # Dataset parameters
-    parser.add_argument('--data-path', default='/datasets01/imagenet_full_size/061417/', type=str,
-                        help='dataset path')
-    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19'],
-                        type=str, help='Image Net dataset path')
-    parser.add_argument('--inat-category', default='name',
-                        choices=['kingdom', 'phylum', 'class', 'order', 'supercategory', 'family', 'genus', 'name'],
-                        type=str, help='semantic granularity')
-
-    parser.add_argument('--output_dir', default='',
+    parser.add_argument('--output_dir', default='./weight',
                         help='path where to save, empty for no saving')
     parser.add_argument('--device', default='cuda',
                         help='device to use for training / testing')
-    parser.add_argument('--seed', default=0, type=int)
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
                         help='start epoch')
-    parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
-    parser.add_argument('--dist-eval', action='store_true', default=True, help='Enabling distributed evaluation')
     parser.add_argument('--num_workers', default=10, type=int)
     parser.add_argument('--pin-mem', action='store_true',
                         help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
@@ -176,16 +103,16 @@ def get_args_parser():
     # distributed training parameters
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
-    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    
+    # * Finetuning params
+    parser.add_argument('--finetune', default='', help='finetune from checkpoint')
     
 
     # NSD dataset and subject information
     parser.add_argument('--subject', default='subj01', help='subject number')
-    parser.add_argument('--roi', default='early',nargs="*", help='Brain ROI')
     parser.add_argument('--fmridir', default='../../testfmri', help='preprocessed fMRI file path')
     parser.add_argument('--featdir', default='../../nsdfeat/subjfeat', help='Image embeddings files from CLIP')
-    parser.add_argument('--kernel', default=10, type=int, help='kernel size')
-    parser.add_argument('--stride', default=7, type=int, help='stride length')
+    parser.add_argument('--patch_size', default=450, type=int, help='patch_size')
     parser.add_argument('--percent', default=1.0, type=float, help='data percent')
     return parser
 
@@ -194,9 +121,6 @@ def main(args):
     utils.init_distributed_mode(args)
 
     print(args)
-
-    if args.distillation_type != 'none' and args.finetune and not args.eval:
-        raise NotImplementedError("Finetuning with distillation not yet supported")
 
     device = torch.device(args.device)
 
@@ -210,16 +134,13 @@ def main(args):
 
     #load fMRI data and image features
     subject=args.subject
-    roi=args.roi
-    kernel_size=args.kernel
-    stride=args.stride
-    fmridir =args.fmridir
-    featdir =args.featdir
+    patch_size=args.patch_size
+
     percent = args.percent
 
 
-    X = np.load(f'/home/students/gzx_4090_1/StableDiffusionReconstruction-main/nsd_fsaverage/{subject}/{subject}_nsdgeneral_betas_tr.npy').astype('float32')
-    X_te = np.load(f'/home/students/gzx_4090_1/StableDiffusionReconstruction-main/nsd_fsaverage/{subject}/{subject}_nsdgeneral_betas_ave_te.npy').astype('float32')
+    X = np.load(f'/home/students/gzx_4090_1/StableDiffusionReconstruction-main/nsd_mrifeat/{subject}/{subject}_nsdgeneral_betas_tr.npy').astype('float32')
+    X_te = np.load(f'/home/students/gzx_4090_1/StableDiffusionReconstruction-main/nsd_mrifeat/{subject}/{subject}_nsdgeneral_betas_ave_te.npy').astype('float32')
     X = torch.tensor(X)
     X_te=torch.tensor(X_te)
 
@@ -236,32 +157,16 @@ def main(args):
         Y = Y[indices]
     
     train_dataset = fMRIimageDataset(X, Y)
-    test_dataset1 = fMRIimageDataset(X_te, Y_te)
+    test_dataset = fMRIimageDataset(X_te, Y_te)
 
 
-    print(f'Now Train model for... {subject}:  {roi}')
+    print(f'Now Train model for... {subject}')
     print(f' X: {X.shape}, Y :{Y.shape}')
 
-    #sampler_train = torch.utils.data.RandomSampler(train_dataset)
-    #sampler_val = torch.utils.data.RandomSampler(test_dataset)
 
-    if True:  # args.distributed:
-        num_tasks = utils.get_world_size()
-        global_rank = utils.get_rank()
-        if args.repeated_aug:
-            sampler_train = RASampler(
-                train_dataset, num_replicas=num_tasks, rank=global_rank, shuffle=True
-            )
-        else:
-            sampler_train = torch.utils.data.DistributedSampler(
-                train_dataset, num_replicas=num_tasks, rank=global_rank, shuffle=True
-            )
+    sampler_train = torch.utils.data.RandomSampler(train_dataset)
+    sampler_val = torch.utils.data.SequentialSampler(test_dataset)
 
-        sampler_val1 = torch.utils.data.SequentialSampler(test_dataset1)
-
-    else:
-        sampler_train = torch.utils.data.RandomSampler(train_dataset)
-        
 
     data_loader_train = torch.utils.data.DataLoader(
         train_dataset, sampler=sampler_train,
@@ -271,8 +176,8 @@ def main(args):
         drop_last=False,
     )
 
-    data_loader_val1 = torch.utils.data.DataLoader(
-        test_dataset1, sampler=sampler_val1,
+    data_loader_val = torch.utils.data.DataLoader(
+        test_dataset, sampler=sampler_val,
         #batch_size=int(1.5 * args.batch_size),
         batch_size=300,
         num_workers=args.num_workers,
@@ -282,9 +187,9 @@ def main(args):
 
 
         
-    model = GFNet1dPyramid(input_size=X.shape[-1], kernel_size=kernel_size, stride=stride, in_chans=1, features=768, embed_dim=[512,256,128,257], depth=[2,10,2,4],
+    model = DFTBackbone(input_size=X.shape[-1], patch_size=patch_size, features=768, embed_dim=[512,256,128,257], depth=[2,10,2,4],
         mlp_ratio=[4, 4, 4, 4], representation_size=None, uniform_drop=False,
-        drop_rate=0.1, drop_path_rate=0.15, norm_layer=partial(nn.LayerNorm, eps=1e-6), 
+        drop_rate=args.drop_rate, drop_path_rate=args.drop_path_rate, norm_layer=partial(nn.LayerNorm, eps=1e-6), 
         dropcls=0
         )
 
@@ -310,19 +215,13 @@ def main(args):
     linear_scaled_lr = args.lr * 600 * utils.get_world_size() / 512.0
     args.lr = linear_scaled_lr
     optimizer = create_optimizer(args, model_without_ddp)
-
     loss_scaler = NativeScaler()
-
     lr_scheduler, _ = create_scheduler(args, optimizer)
-
-
-
     output_dir = Path(args.output_dir)
 
 
-    print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
-    max_test1=-9999
+    max_test=-9999
 
     for epoch in range(args.start_epoch, args.epochs):
 
@@ -333,28 +232,28 @@ def main(args):
             model, data_loader_train,
             optimizer, device, epoch, loss_scaler,
             args.clip_grad, model_ema, mixup_fn=None,
-            set_training_mode=args.finetune == ''  # keep in eval mode during finetuning
+            set_training_mode=args.finetune == '' 
         )
 
         lr_scheduler.step(epoch)
 
-        test_stats1, loss_mlp = evaluate(data_loader_val1, model, device)
+        test_stats = evaluate(data_loader_val, model, device)
 
 
 
-        if test_stats1>=max_test1:
-            max_test1=test_stats1
-            torch.save(model.state_dict(),os.path.join(output_dir,f'GFNet-besttest-subj01.pth'))
-            max_epoch1=epoch
+        if test_stats>=max_test:
+            max_test=test_stats
+            torch.save(model.state_dict(),os.path.join(output_dir,f'litemind-{subject}.pth'))
+            max_epoch=epoch
         
             
 
         lr=train_stats['lr']
         loss=train_stats['loss']
-        log_stats = f'train_lr:{lr},train_loss:{loss:.5f},epoch:{epoch},n_parameters:{n_parameters},Top1:{test_stats1/982*100:.2f}%, mse:{loss_mlp:.4f}, epochmax:{max_epoch1}'
+        log_stats = f'train_lr:{lr},train_loss:{loss:.5f},epoch:{epoch},n_parameters:{n_parameters},Top1:{test_stats1/982*100:.2f}%, epochmax:{max_epoch}'
 
         if args.output_dir and utils.is_main_process():
-            with (output_dir / "log.txt").open("a") as f:
+            with ("./log/{subject}_log.txt").open("a") as f:
                 f.write(log_stats+ "\n")
 
     total_time = time.time() - start_time
@@ -363,7 +262,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Lit-Mind training script', parents=[get_args_parser()])
+    parser = argparse.ArgumentParser('Lite-Mind training script', parents=[get_args_parser()])
     args = parser.parse_args()
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
